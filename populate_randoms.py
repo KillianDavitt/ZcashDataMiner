b468dcdbc0cf5bbc315d9613b3d8622fbb619af1
Killian <davittk@tcd.ie>
2018-03-05 15:21:01 +0000

    Populate randoms is working

--- populate_randoms.py
+++ populate_randoms.py
@@ -8,15 +8,21 @@ n = None
 with open('progress.txt') as f:
     n = int(f.read())
 
+
+# For every vin script
 for i in range(5000000):
     c.execute('SELECT * FROM "vin_script" WHERE script_id=' + str(n) + ';')
     scr = c.fetchone()
+
+    # Split the script by space
     s_r = scr[2].split(' ')[0]
     s = s_r[0:int(len(s_r)/2)]
     r = s_r[int(len(s_r)/2):]
     sig = Signature(r=r, s=s, tx_id=scr[6])
     db.session.add(sig)
     n = n+1
+
+    ## Save progress once in a while
     if n%1000 == 0:
         db.session.commit()
         print(n)
